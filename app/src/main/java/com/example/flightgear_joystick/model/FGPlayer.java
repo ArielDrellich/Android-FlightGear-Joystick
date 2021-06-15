5ad43be632ec3fa4ff9776b9864bfc080ed8e7fb
ArielDrellich <arieldrellich@gmail.com>
2021-06-15 15:19:56 +0300

    socket and threadpool logic

--- app/src/main/java/com/example/flightgear_joystick/model/FGPlayer.java
+++ app/src/main/java/com/example/flightgear_joystick/model/FGPlayer.java
@@ -1,20 +1,54 @@
 package com.example.flightgear_joystick.model;
 
-import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.SocketOption;
-import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FGPlayer {
     Socket fg;
+    PrintWriter out;
+    ExecutorService executor;
 
-    public void startConnection(String host, int port) {
+    public FGPlayer(String host, int port) {
         try {
-            fg = new Socket("localhost", 5400);
-//            fg = new Socket(host, port);
+            fg = new Socket(host, port);
+            out = new PrintWriter(fg.getOutputStream(),true);
+            executor = Executors.newSingleThreadExecutor();
         } catch (Exception e) {
-            System.out.println("Error opening socket");
-            e.printStackTrace();
+            System.out.println("Error opening socket: " + e);
+        }
+    }
+
+    public void sendDataToFG(String field, double value) {
+        switch (field) {
+            case "aileron":
+                executor.execute(() -> {
+                    out.print("set /controls/flight/aileron " + value + "\r\n");
+                    out.flush();
+                });
+                break;
+
+            case "elevator":
+                executor.execute(() -> {
+                    out.print("set /controls/flight/elevator " + value + "\r\n");
+                    out.flush();
+                });
+                break;
+
+            case "rudder":
+                executor.execute(() -> {
+                    out.print("set /controls/flight/rudder " + value + "\r\n");
+                    out.flush();
+                });
+                break;
+
+            case "throttle":
+                executor.execute(() -> {
+                    out.print("set /controls/engines/current-engine/throttle " + value + "\r\n");
+                    out.flush();
+                });
+                break;
         }
     }
 
